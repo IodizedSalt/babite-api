53eb71994e863334167b2e347c8bf062cceab579
christopher <christopher@procedural.build>
2020-05-12 12:24:33 +0200

    update create channel

--- create-channels.js
+++ create-channels.js
@@ -6,22 +6,48 @@ const documentClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async event => {
   const { category, name, description, userUUID, thumbnail, mature } = JSON.parse(event.body)
-  const params = {
+  
+    const existing_channel = {
     TableName: "channels",
-    Item: {
+    Key: { name },         
+    Item: { 
       name: name,
-      userUUID: userUUID,
-      category: category,
-      description: description,
-      thumbnail: thumbnail,
-      mature: mature
+      userUUID: userUUID
     }
   }
-  try {
-    const data = await documentClient.put(params).promise()
-    return { response: 'Channel created successfully', statusCode: 200 }
-  } catch (e) {
-    return { statusCode: 500, body: JSON.stringify(e)
-  }
-  }
+    let result = await documentClient.get(existing_channel).promise();
+    let empty_object_check = result.Item !== undefined && result.Item !== null
+    
+    // Check if database returned item is empty or if already exists under userUUID
+    if (empty_object_check && result.Item.userUUID == userUUID) {
+      return { statusCode: 500, body: ("CHANNEL EXISTS ALREADY")}
+      
+    }else if(empty_object_check&& result.Item.userUUID !== userUUID){
+      return { statusCode: 500, body: ("CHANNEL EXISTS UNDER OTHER USER ALREADY")}
+      
+    }else{
+      const params = {
+        TableName: "channels",
+        Item: { 
+          name: name,
+          userUUID: userUUID,
+          category: category,
+          description: description,
+          thumbnail: thumbnail,
+          mature: mature
+        }
+      }
+      try {
+        const data = await documentClient.put(params).promise()
+        return { response: 'Channel created successfully', statusCode: 200 }
+      } catch (e) {
+        return { statusCode: 500, body: JSON.stringify(e)
+      }
+      }    
+      
+    }
+  
+  
+  
+
 }
